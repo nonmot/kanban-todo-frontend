558b30995531ffc85c87ee325f94be161e1f802b
Motoya Nonaka <69344127+nonmot@users.noreply.github.com>
2025-10-09 19:59:27 +0900

    Feature/header (#2)
    * feature: add header to todo page
    
    * add header
    
    * fix
    
    ---------
    
    Co-authored-by: Motoya Nonaka <Motoya.Nonaka@ibm.com>

---
+++ src/app/todos/layout.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/Header"
+
+export default async function TodosLayout({ children }: { children: React.ReactNode}) {
+  return (
+    <div className="container mx-auto">
+      <Header />
+      {children}
+    </div>
+  )
+}
